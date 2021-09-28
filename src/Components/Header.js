a5b3ecbc327002e08e746cf9226e2136d2aae9ca
Nyawira Muturi <albinamuturi@gmail.com>
2021-09-28 15:57:18 +0300

    work around props, css, and inline styling with buttons

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Button from './Button'
 
 const Header = () => {
     return (
-        <header>
+        <header className='header'>
         <h1>Task Manager</h1>
+        <Button color='green' text='ADD' />
         </header>
     )
 }
+
 
 export default Header
