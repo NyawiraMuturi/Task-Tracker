6dca777eab679504bb932955e517bedc04778de2
Nyawira Muturi <albinamuturi@gmail.com>
2021-09-28 16:12:13 +0300

    explore click events and working across components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Button from './Button'
 
 const Header = () => {
+    const onClick = ()=> {
+        alert('Hey, I am learning react')
+    }
+
     return (
         <header className='header'>
         <h1>Task Manager</h1>
-        <Button color='green' text='ADD' />
+        <Button color='green' text='ADD' onClick={onClick} />
         </header>
     )
 }
